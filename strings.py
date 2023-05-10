8118080ecc61bb25acfc6f6198cc77b3b6e480cc
Ray Hong <rayhong24@gmail.com>
2023-05-10 14:43:44 -0700

    Changed error messages

--- strings.py
+++ strings.py
@@ -11,7 +11,9 @@ invalid_move_no_piece_message = invalid_move_template.format("No piece to move o
 
 # Message used when trying to move a piece of the wrong colour
 # Format with:
-# piece - piece
+# colour1 - colour of the piece you are trying to move
+# colour2 - colour of the piece that is actually on the square
 # i - index of row
 # j - index of column
-invalid_move_wrong_colour = invalid_move_template.format("Cannot move piece of other colour.")
+invalid_move_wrong_piece = invalid_move_template.format("There is a piece mismatch on {}, {}. Actualy representation is {} while the piece you are trying to move is {}.")
+
