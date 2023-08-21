3299e479a6839ecc669951a4cba09bc10bdb0126
Ray Hong <rayhong24@gmail.com>
2023-08-21 12:50:05 -0700

    Revert "Add board as an attribute of piece"
    This reverts commit 61b9fe78dd6f733f2777364057086832b143e7df.

--- Pieces/bishop.py
+++ Pieces/bishop.py
@@ -2,7 +2,7 @@ from Pieces.piece import Piece
 from enums import Colour
 
 class Bishop(Piece):
-    def __init__(self, board, colour, row, column):
-        super().__init__(board, colour, row, column)
+    def __init__(self, colour, row, column):
+        super().__init__(colour, row, column)
     def get_representation(self):
         return 'b' if self.colour == Colour.BLACK else 'B'
--- Pieces/piece.py
+++ Pieces/piece.py
@@ -2,8 +2,7 @@ from enums import Colour
 from strings import *
 
 class Piece:
-    def __init__(self, board, colour, row, column):
-        self.board = board
+    def __init__(self, colour, row, column):
         self.colour = colour
         self.row = row
         self.column = column
@@ -11,25 +10,25 @@ class Piece:
     def get_representation(self):
         raise NotImplementedError
 
-    def get_moves(self):
+    def get_moves(self, board):
         raise NotImplementedError
     
-    def move(self, new_i, new_j):
-        self.check_move_errors(self.get_representation())               
+    def move(self, board, new_i, new_j):
+        self.check_move_errors(board, self.get_representation())               
         
         self.row = new_i
         self.column = new_j
     
-    def check_move_errors(self, representation):
-        if self.board.board[self.row][self.column] is None:
+    def check_move_errors(self, board, representation):
+        if board[self.row][self.column] is None:
             raise Exception(invalid_move_no_piece_message.format(\
             self.row,\
             self.column))
-        elif self.board.board[self.row][self.column].get_representation() != representation:
+        elif board[self.row][self.column].get_representation() != representation:
             raise Exception(invalid_move_wrong_piece.format(\
             self.row,\
             self.column,\
-            self.board[self.row][self.column].get_representation(), \
+            board[self.row][self.column].get_representation(), \
             representation))
 
 
