0645dd10ee63463e39aead9a3dccd71ea621c0ce
Ray Hong <rayhong24@gmail.com>
2023-08-20 21:02:49 -0700

    Update pawn tests

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import game
 from board import Board
 
-#TODO: add verbose flag
-def pawn_test():
-    print("Testing Pawn implementation")
-    forward_test_board = Board()
-    forward_test_board.print_board()
-    i, j = 1, 1
+def keep_making_first_move(board, piece):
     while True:
-        moves = forward_test_board.board[i][j].get_moves(forward_test_board.board)
+        old_i, old_j = piece.row, piece.column
+        moves = piece.get_moves(board.board)
 
         if not moves:
             print("No more valid moves")
             break
 
-        ii, jj = moves[0]
-        forward_test_board.move_piece(i, j, ii, jj)
-        i, j = ii, jj
-        forward_test_board.print_board()
+        new_i, new_j = moves[0]
+        board.move_piece(old_i, old_j, new_i, new_j)
+        board.print_board()
+
+    return
+
+
+#TODO: add verbose flag
+def pawn_test():
+    print("Testing Pawn implementation")
+    forward_test_board = Board()
+    forward_test_board.print_board()
+
+    print("Moving pawn 1")
+    pawn = forward_test_board.board[1][1]
+    keep_making_first_move(forward_test_board, pawn)
+    
+    print("Moving pawn 2")
+    pawn2 = forward_test_board.board[6][4]
+    keep_making_first_move(forward_test_board, pawn2)
     
 def main():
     pawn_test()
