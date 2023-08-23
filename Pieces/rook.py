62bf13d117b1505af25ebab7ff34148e443a6714
Ray Hong <rayhong24@gmail.com>
2023-08-23 16:33:43 -0700

    Add type hints

--- Pieces/rook.py
+++ Pieces/rook.py
@@ -1,14 +1,16 @@
+from __future__ import annotations
+
 from Pieces.piece import Piece
 from enums import Colour
 
 class Rook(Piece):
-    def __init__(self, colour, row, column):
+    def __init__(self, colour: Colour, row: int, column: int) -> None:
         super().__init__(colour, row, column)
 
-    def get_representation(self):
+    def get_representation(self) -> str:
         return 'r' if self.colour == Colour.BLACK else 'R'
 
-    def get_moves(self, board):
+    def get_moves(self, board: Board) -> [tuple]:
         super().check_move_errors(board, self.get_representation())
         # list of tuples of new coordinates the piece can go
         valid_moves = []
