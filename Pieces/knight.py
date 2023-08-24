31c9d980cd3f4bed1e29d6f0eb091ed115e3f0be
Ray Hong <rayhong24@gmail.com>
2023-08-23 21:30:55 -0700

    Implement get_moves() for knight

--- Pieces/knight.py
+++ Pieces/knight.py
@@ -1,8 +1,27 @@
+from __future__ import annotations
+
 from Pieces.piece import Piece
 from enums import Colour
 
 class Knight(Piece):
-    def __init__(self, colour, row, column):
+    def __init__(self, colour: Colour, row: int, column: int) -> None:
         super().__init__(colour, row, column)
-    def get_representation(self):
+
+    def get_representation(self) -> str:
         return 'n' if self.colour == Colour.BLACK else 'N'
+
+    def get_moves(self, board: Board) -> [tuple]:
+        super().check_move_errors(board, self.get_representation())
+        # list of tuples of new coordinates the piece can go
+        valid_moves = []
+
+        for di, dj in [
+            [-2, -1], [-2, 1], [-1, -2], [-1, 2],
+            [1, -2], [1, 2], [2, -1], [2, 1]
+            ]:
+            i, j = self.row + di, self.column + dj
+
+            if board.is_inbounds(i, j) and (board.board[i][j] == None or board.board[i][j].colour != self.colour):
+                valid_moves.append((i, j))
+        
+        return valid_moves
