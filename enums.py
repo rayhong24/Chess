c959241b812077d3f128fb95fc322931cad1f592
Ray Hong <rayhong24@gmail.com>
2023-08-26 09:17:50 -0700

    Change files to use lowercase letters

--- enums.py
+++ enums.py
@@ -1,14 +1,14 @@
 from enum import Enum
 
 class File(Enum):
-    A = 0
-    B = 1
-    C = 2
-    D = 3
-    E = 4
-    F = 5
-    G = 6
-    H = 7
+    a = 0
+    b = 1
+    c = 2
+    d = 3
+    e = 4
+    f = 5
+    g = 6
+    h = 7
 
 class Colour(Enum):
     BLACK = 0
