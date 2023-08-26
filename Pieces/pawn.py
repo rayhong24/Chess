cf2290eafc574a482f0da7edf36262758ea735b6
Ray Hong <rayhong24@gmail.com>
2023-08-26 11:24:21 -0700

    Basic implementation of promotion

--- Pieces/pawn.py
+++ Pieces/pawn.py
@@ -9,6 +9,10 @@ class Pawn(Piece):
         return 'p' if self.colour == Colour.BLACK else 'P'
 
     def get_moves(self, board: [[Piece]]) -> [str]:
+        def append_promotion_move(move_str):
+            for piece in "qrbn":
+                valid_moves.append(move_str+piece)
+
         # list of tuples of new coordinates the piece can go
         valid_moves = []
 
@@ -19,7 +23,12 @@ class Pawn(Piece):
         for di in range(1, moves_forward+1):
             i, j = self.row+(di*direction), self.column
             if self.is_inbounds(i, j) and board[i][j] == None:
-                valid_moves.append(self.coords_to_move(self.row, self.column, i, j))
+                move_str = self.get_move_str(self.row, self.column, i, j)
+                # For promotions
+                if i == 0 or i == 7:
+                    append_promotion_move(move_str)
+                else:
+                    valid_moves.append(move_str)
         
         # check captures
         i = self.row+direction
@@ -28,12 +37,22 @@ class Pawn(Piece):
         if self.is_inbounds(i, j_left):
             square_to_check1 = board[i][j_left]
             if square_to_check1 is not None and square_to_check1.colour != self.colour:
-                valid_moves.append(self.coords_to_move(self.row, self.column, i, j_left))
+                move_str = self.get_move_str(self.row, self.column, i, j_left)
+                # For promotions
+                if i == 0 or i == 7:
+                    append_promotion_move(move_str)
+                else:
+                    valid_moves.append(move_str)
         
         if self.is_inbounds(i, j_right):
             square_to_check2 = board[i][j_right]
             if  square_to_check2 is not None and square_to_check2.colour != self.colour:
-                valid_moves.append(self.coords_to_move(self.row, self.column, i, j_right))
+                move_str = self.get_move_str(self.row, self.column, i, j_right)
+                # For promotions
+                if i == 0 or i == 7:
+                    append_promotion_move(move_str)
+                else:
+                    valid_moves.append(move_str)
 
         # TODO: En passant
         
