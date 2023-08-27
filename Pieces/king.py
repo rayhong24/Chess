cc148c31f707458a6e08d4f8e184d25ecb43767a
Ray Hong <rayhong24@gmail.com>
2023-08-27 11:44:39 -0700

    Naive implement castling (no testing yet)

--- Pieces/king.py
+++ Pieces/king.py
@@ -1,4 +1,5 @@
 from Pieces.piece import Piece
+from Pieces.rook import Rook
 from enums import Colour
 
 class King(Piece):
@@ -20,5 +21,16 @@ class King(Piece):
 
             if self.is_inbounds(i, j) and (board[i][j] == None or board[i][j].colour != self.colour):
                 valid_moves.append(self.get_move_str(self.row, self.column, i, j))
+
+        # Check king side castle
+        if self.column == 4 and not self.has_moved \
+            and board[self.row][5] == None and board[self.row][6] == None\
+            and type(board[self.row][7]) == Rook and not board[self.row][7].has_moved:
+                valid_moves.append(self.get_move_str(self.row, self.column, self.row, 6))
+
+        if self.column == 4 and not self.has_moved \
+            and board[self.row][3] == None and board[self.row][2] == None and board[self.row][1] == None\
+            and type(board[self.row][0]) == Rook and not board[self.row][0].has_moved:
+                valid_moves.append(self.get_move_str(self.row, self.column, self.row, 2))
         
         return valid_moves
