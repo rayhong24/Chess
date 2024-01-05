4fb80c9de46fd74efb47b8843637e1ae5287ebf5
ray hong <rayhong24@github.com>
2024-01-04 23:36:52 -0800

    Add Moves class and a MovesFactory

---
+++ Moves/moveFactory.py
@@ -0,0 +1,84 @@
+from utils import *
+from Moves.move import Move
+from Moves.castle import Castle
+from Moves.promotion import Promotion
+from utils import Colour
+
+class MoveFactory():
+    def init_move(self, move_str: str, player_to_move: Colour) -> Move:
+        initializer = self._get_move_initializer(move_str)
+
+        return initializer(move_str, player_to_move)
+
+    def _get_move_initializer(self, move_str):
+        if move_str == "O-O" or move_str == "O-O-O":
+            return self._init_castle
+        elif "=" in move_str:
+             return self._init_promotion
+        else:
+            return self._init_normal
+    
+    def _split_move_str(self, move_str):
+        piece = 'P'
+        if "-" in move_str:
+            start, end = move_str.split('-')
+
+            if len(start) == 3:
+                piece = start[0]
+                start_i, start_j = to_coords(start[1:])
+            else:
+                start_i, start_j = to_coords(start[:2])
+
+            capture = False
+            end_i, end_j = to_coords(end[:2])
+        elif "x" in move_str:
+            start, end = move_str.split('x')
+
+            if len(start) == 3:
+                piece = start[0]
+                start_i, start_j = to_coords(start[1:])
+            else:
+                start_i, start_j = to_coords(start[:2])
+
+            capture = True
+            end_i, end_j = to_coords(end[:2])
+        else:
+            raise ValueError("Invalid move.")
+
+        return (piece,
+            (start_i, start_j),
+            capture,
+            (end_i, end_j)
+        )
+    
+    def _init_normal(self, move_str: str, player_to_move: Colour) -> Move:
+        piece, start_coords, capture, end_coords = self._split_move_str(move_str)
+
+        return Move(
+            player_to_move,
+            piece,
+            start_coords,
+            capture,
+            end_coords
+        )
+
+
+    def _init_castle(self, move_str: str, player_to_move: Colour) -> Move:
+        return Castle(
+            move_str,
+            player_to_move,
+        )
+
+    def _init_promotion(self, move_str: str, player_to_move: Colour) -> Move:
+        move, promotion_piece = move_str.split('=')
+        _, start_coords, capture, end_coords = self._split_move_str(move)
+
+        return Promotion(
+            player_to_move,
+            'P',
+            start_coords,
+            capture,
+            end_coords,
+            promotion_piece
+        )
+
--- manual_testing.py
+++ manual_testing.py
@@ -1,24 +1,17 @@
 import unittest
 
+from enums import *
 from game import Game
 from board import Board
 from Pieces.piece import Piece
+from Moves.moveFactory import MoveFactory
 
 def main():
-    game = Game()
-    game.display_game()
-    game.board.handle_move("e2-e4")
-    game.display_game()
-    game.board.handle_move("d7-d5")
-    game.display_game()
-    game.board.handle_move("e4xd5")
-    game.display_game()
-    game.board.handle_move("d5-d6")
-    game.display_game()
-    game.board.handle_move("d6-d7")
-    game.display_game()
-    game.board.handle_move("d7xc8=Q")
-    game.display_game()
+    factory = MoveFactory()
+    m = factory.init_move("e29-b232", Colour.WHITE)
+    print(f"{m=}")
+
+
 
 if __name__ == "__main__":
     main()
