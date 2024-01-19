61c56b93f79524d7e00c6034746249964ba6d08f
Ray Hong <rayhong24@gmail.com>
2024-01-19 13:03:40 -0800

    Add testcases and create a shell testcase in testPiece superclass

--- Tests/testPawn.py
+++ Tests/testPawn.py
@@ -7,12 +7,15 @@ class TestPawn(PiecesTestCases.TestPieces):
     def setUp(self):
         return super().setUp()
 
-    def test_e_pawn_simple(self):
+    def test_starting_position(self):
         self.game.setup_fenstr()
-        piece = self.game.board.board[6][4]
-        expected_moves = [
-            self.move_factory.init_move("e2-e3", Colour.WHITE),
-            self.move_factory.init_move("e2-e4", Colour.WHITE)
-        ]
 
-        self.check_moves(piece, expected_moves)
+        for i in range(8):
+            piece = self.game.board.board[6][i]
+            file_label = File(i).name
+            expected_moves = [
+                self.move_factory.init_move(f"{file_label}2-{file_label}3", Colour.WHITE),
+                self.move_factory.init_move(f"{file_label}2-{file_label}4", Colour.WHITE)
+            ]
+
+            self.check_moves(piece, expected_moves)
