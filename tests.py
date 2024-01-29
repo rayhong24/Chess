cdfa7b8a103e3810c78af2749c629cf90929e693
Ray Hong <rayhong24@gmail.com>
2024-01-29 15:06:00 -0800

    Move tests into folders

--- tests.py
+++ tests.py
@@ -1,12 +1,21 @@
 import unittest
 
 def main():
-    loader = unittest.TestLoader()
-    start_dir = 'Tests'
-    suite = loader.discover(start_dir)
-
     runner = unittest.TextTestRunner()
-    runner.run(suite)
+
+    piece_dir = 'Tests/TestPieces'
+    piece_loader = unittest.TestLoader()
+    piece_suite = piece_loader.discover(piece_dir)
+
+    print(f"\nTesting dir: {piece_dir}")
+    runner.run(piece_suite)
+
+    game_dir = 'Tests/TestGame'
+    game_loader = unittest.TestLoader()
+    game_suite = game_loader.discover(game_dir)
+
+    print(f"\nTesting dir: {game_dir}")
+    runner.run(game_suite)
     return
 
 if __name__ == "__main__":
