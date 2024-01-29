2c0e691cb4b1f8aa0b3d5faa646e100fe83dff84
Ray Hong <rayhong24@gmail.com>
2024-01-29 15:54:49 -0800

    Add rook check test cases

--- Tests/TestGame/testCheck.py
+++ Tests/TestGame/testCheck.py
@@ -19,7 +19,7 @@ class TestCheck(GameTestCases.TestGameClass):
         self.check_board_equal(b)
         self.check_move(Colour.BLACK)
         self.check_castling_rights("-")
-        self.assertEqual(False, self.game.is_king_in_check(Colour.WHITE))
+        self.assertFalse(self.game.is_king_in_check(Colour.WHITE))
 
         move = self.move_factory.init_move("e7-e5", self.game.player_turn)
         move.make_move(self.game)
@@ -38,7 +38,7 @@ class TestCheck(GameTestCases.TestGameClass):
         self.check_board_equal(b)
         self.check_move(Colour.WHITE)
         self.check_castling_rights("-")
-        self.assertEqual(True, self.game.is_king_in_check(Colour.WHITE))
+        self.assertTrue(self.game.is_king_in_check(Colour.WHITE))
 
     def test_check_with_pawn_black(self):
         self.game.setup_fenstr("8/8/8/3k4/8/8/4P3/8 w - - 0 1")
@@ -57,7 +57,7 @@ class TestCheck(GameTestCases.TestGameClass):
         self.check_board_equal(b)
         self.check_move(Colour.WHITE)
         self.check_castling_rights("-")
-        self.assertEqual(False, self.game.is_king_in_check(Colour.BLACK))
+        self.assertFalse(self.game.is_king_in_check(Colour.BLACK))
 
         move = self.move_factory.init_move("e2-e4", self.game.player_turn)
         move.make_move(self.game)
@@ -76,4 +76,80 @@ class TestCheck(GameTestCases.TestGameClass):
         self.check_board_equal(b)
         self.check_move(Colour.BLACK)
         self.check_castling_rights("-")
-        self.assertEqual(True, self.game.is_king_in_check(Colour.BLACK))
+        self.assertTrue(self.game.is_king_in_check(Colour.BLACK))
+    
+    def test_check_with_rook_white(self):
+        self.game.setup_fenstr("r7/8/8/8/4K3/8/8/8 b - - 0 1")
+
+        b = [
+                ['r','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','K','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','','']
+            ]
+
+        self.check_board_equal(b)
+        self.check_move(Colour.BLACK)
+        self.check_castling_rights("-")
+        self.assertFalse(self.game.is_king_in_check(Colour.WHITE))
+
+        move = self.move_factory.init_move("Ra8-a4", self.game.player_turn)
+        move.make_move(self.game)
+
+        b = [
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['r','','','','K','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','','']
+            ]
+
+        self.check_board_equal(b)
+        self.check_move(Colour.WHITE)
+        self.check_castling_rights("-")
+        self.assertTrue(self.game.is_king_in_check(Colour.WHITE))
+
+    def test_check_with_rook_black(self):
+        self.game.setup_fenstr("8/8/8/8/4k3/8/8/R7 w - - 0 1")
+
+        b = [
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','k','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['R','','','','','','','']
+            ]
+
+        self.check_board_equal(b)
+        self.check_move(Colour.WHITE)
+        self.check_castling_rights("-")
+        self.assertFalse(self.game.is_king_in_check(Colour.BLACK))
+
+        move = self.move_factory.init_move("Ra1-a4", self.game.player_turn)
+        move.make_move(self.game)
+
+        b = [
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['R','','','','k','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','','']
+            ]
+
+        self.check_board_equal(b)
+        self.check_move(Colour.BLACK)
+        self.check_castling_rights("-")
+        self.assertTrue(self.game.is_king_in_check(Colour.BLACK))
