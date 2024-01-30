f948904850340b69a28b4d2df407d180c5f59867
Ray Hong <rayhong24@gmail.com>
2024-01-29 16:06:56 -0800

    Add check with knight tests

--- Tests/TestGame/testCheck.py
+++ Tests/TestGame/testCheck.py
@@ -153,3 +153,79 @@ class TestCheck(GameTestCases.TestGameClass):
         self.check_move(Colour.BLACK)
         self.check_castling_rights("-")
         self.assertTrue(self.game.is_king_in_check(Colour.BLACK))
+
+    def test_check_with_knight_white(self):
+        self.game.setup_fenstr("1n6/8/8/8/3K4/8/8/8 b - - 0 1")
+
+        b = [
+                ['','n','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','K','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','','']
+            ]
+
+        self.check_board_equal(b)
+        self.check_move(Colour.BLACK)
+        self.check_castling_rights("-")
+        self.assertFalse(self.game.is_king_in_check(Colour.WHITE))
+
+        move = self.move_factory.init_move("Nb8-c6", self.game.player_turn)
+        move.make_move(self.game)
+
+        b = [
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','n','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','K','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','','']
+            ]
+
+        self.check_board_equal(b)
+        self.check_move(Colour.WHITE)
+        self.check_castling_rights("-")
+        self.assertTrue(self.game.is_king_in_check(Colour.WHITE))
+
+    def test_check_with_knigh_black(self):
+        self.game.setup_fenstr("8/8/8/8/4k3/8/8/1N6 w - - 0 1")
+
+        b = [
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','k','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','N','','','','','','']
+            ]
+
+        self.check_board_equal(b)
+        self.check_move(Colour.WHITE)
+        self.check_castling_rights("-")
+        self.assertFalse(self.game.is_king_in_check(Colour.BLACK))
+
+        move = self.move_factory.init_move("Nb1-c3", self.game.player_turn)
+        move.make_move(self.game)
+
+        b = [
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','k','','',''],\
+                ['','','N','','','','',''],\
+                ['','','','','','','',''],\
+                ['','','','','','','','']
+            ]
+
+        self.check_board_equal(b)
+        self.check_move(Colour.BLACK)
+        self.check_castling_rights("-")
+        self.assertTrue(self.game.is_king_in_check(Colour.BLACK))
