d557b2b4706bcd521e55784af250aa0ef2cbcd7a
Ray Hong <rayhong24@gmail.com>
2024-02-26 12:03:26 -0800

    Fix enpassant when using interface. Fix import error.

--- player.py
+++ player.py
@@ -26,7 +26,7 @@ class Player():
             usr_input = self.interface.get_player_input(game)
 
             try:
-                chosen_move = self.move_factory.init_move(usr_input, self.colour)
+                chosen_move = self.move_factory.init_move(usr_input, self.colour, game)
 
             except:
                 print("Illegal move. Try again")
