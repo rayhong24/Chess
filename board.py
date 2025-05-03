c06636c3eb4aee51fa242f6373fa8b384d118056
Ray Hong <rayhong24@gmail.com>
2025-05-03 09:28:17 -0700

    Make board variables private. Provide functions to probe state of board instead

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+import Pieces
+
 from strings import *
 from enums import *
 from coords import Coords
@@ -6,36 +8,36 @@ from Pieces.pieceFactory import PieceFactory
 
 class Board:
     def __init__(self):
-        self.piece_factory = PieceFactory()
-        self.board = [[None]*8 for _ in range(8)]
+        self._piece_factory = PieceFactory()
+        self._board = [[None]*8 for _ in range(8)]
 
-    def get_square(self, coords: Coords):
-        return self.board[8-coords.rank][coords.file.value]
+    def get_square(self, coords: Coords) -> Pieces:
+        return self._board[8-coords.rank][coords.file.value]
 
     def set_square(self, value, coords: Coords):
-        self.board[8-coords.rank][coords.file.value] = value
+        self._board[8-coords.rank][coords.file.value] = value
 
     # Input: string from a fenstring (i.e. rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR)
-    def add_fenstr_pieces(self, s: str) -> None:
+    def set_fenstr(self, s: str) -> None:
         for i, row in enumerate(s.split('/')):
             j = 0
             for c in row:
                 if c.isnumeric():
                     for _ in range(int(c)):
-                        self.board[i][j] = None
+                        self._board[i][j] = None
                         j += 1
                 else:
                     j += 1
 
 
-    # def get_square_representation(self, val) -> str:
-    #     if val is None:
-    #         return ""
-    #     else:
-    #         return val.get_representation()
+    def get_square_representation(self, val) -> str:
+        if val is None:
+            return ""
+        else:
+            return val.get_representation()
 
     def print_board(self):
-        for i, row in enumerate(self.board):
+        for i, row in enumerate(self._board):
             print(f"{8-i} {['{:^3}'.format(self.get_square_representation(val)) for val in row]}")
         print()
         print(f"  {['{:^3}'.format(File(i).name) for i in range(8)]}")
