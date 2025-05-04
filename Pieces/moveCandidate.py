d4d88bb0c7dda96f92aef06f0072aaa071a2ad74
Ray Hong <rayhong24@gmail.com>
2025-05-03 18:26:24 -0700

    Modify moveCandidate class to include allow capture and forced capture

--- Pieces/moveCandidate.py
+++ Pieces/moveCandidate.py
@@ -1,10 +1,12 @@
 class MoveCandidate():
-    def __init__(self, capture, row_diff, col_diff, dist=1):
-        self.capture = capture
+    def __init__(self, row_diff, col_diff, dist=1, capture_allowed = False, capture_forced = False):
         self._row_diff: int = row_diff
         self._col_diff: int = col_diff
 
         self._dist = dist
+
+        self.capture_allowed = capture_allowed
+        self.capture_forced = capture_forced
 
     def __repr__(self):
         return self.__str__()
