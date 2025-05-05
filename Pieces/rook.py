e97df9cc83e13e03c7f48e718c0e9139977fde02
Ray Hong <rayhong24@gmail.com>
2025-05-05 11:08:26 -0700

    Use move candidates in Rook class

--- Pieces/rook.py
+++ Pieces/rook.py
@@ -1,4 +1,6 @@
 from Pieces.piece import Piece
+
+from Pieces.moveCandidate import MoveCandidate
 from coords import Coords
 from enums import Colour
 
@@ -9,28 +11,16 @@ class Rook(Piece):
     def get_representation(self) -> str:
         return 'r' if self.colour == Colour.BLACK else 'R'
 
-    def get_moves(self, game) -> [str]:
+    def get_candidate_moves(self) -> [str]:
         # list of tuples of new coordinates the piece can go
-        valid_moves = []
+        candidates = []
 
         for di, dj in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-            for line_coords in self.coords.get_line(di, dj):
-                square = game.board.get_square(line_coords)
-                move = self.move_factory.init_normal_move(
-                    self.colour,
-                    'R',
-                    self.coords,
-                    square is not None,
-                    line_coords
-                )
-                if square is not None:
-                    if square.colour != self.colour:
-                        valid_moves.append(move)
-                    break
-                valid_moves.append(move)
+            candidate = MoveCandidate(di, dj, 8)
 
+            candidates.append(candidate)
             
-        return valid_moves
+        return candidates
 
             
 
