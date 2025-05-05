d1177a4f09f1a5709a19af15500000bd9ea594d9
ray hong <rayhong24@github.com>
2025-05-05 11:08:26 -0700

    Use move candidates in Rook class

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ from Moves.move import Move
 
 
 class Game():
-    startpos_fenstr = "b7/pppppppp/8/8/8/8/P1PPPPPP/BN6 w KQkq - 0 1"
+    startpos_fenstr = "b7/pppppppp/8/8/8/8/P11PPPPP/BNR5 w KQkq - 0 1"
     # startpos_fenstr = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
     def __init__(self, fenstr=startpos_fenstr):
         self.board = Board()
