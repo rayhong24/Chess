1b87bc929a0667efb2cc1258c457940299b2fa39
ray hong <rayhong24@github.com>
2025-05-06 14:06:12 -0700

    Remove unused move classes

--- Engines/minimax.py
+++ Engines/minimax.py
@@ -4,5 +4,5 @@ class Minimax(Engine):
     def __init__(self):
         super().__init__()
 
-    # def go(self, depth=2):
-    #     if depth == 0 or 
+    def go(self, depth=2):
+        if depth == 0 or 
