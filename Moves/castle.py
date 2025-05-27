725294a65e1b0248837af946b19721b1cfad7b26
ray hong <rayhong24@github.com>
2025-05-26 22:56:10 -0700

    Fix minor bugs in castling logic. Have not fully implemented proper castling checks yet

--- Moves/castle.py
+++ Moves/castle.py
@@ -1,11 +1,28 @@
 from GameClasses.board import Board
 from Moves.move import Move
-class Castle(Move):
-    def __init__(self, player_to_move, start_coords, capture, end_coords, rook_start_coords, rook_end_coords):
-        super().__init__(player_to_move, start_coords, capture, end_coords)
 
-        self.rook_start_coords = rook_start_coords
-        self.rook_end_coords = rook_end_coords
+from coords import Coords
+
+class Castle(Move):
+    def __init__(self, player_to_move, start_coords, end_coords):
+        super().__init__(player_to_move, start_coords, False, end_coords)
+
+        if str(start_coords) == "e1":
+            if str(end_coords) == "g1":
+                self.rook_start_coords = Coords.init_from_str("h1")
+                self.rook_end_coords = Coords.init_from_str("f1")
+            elif str(end_coords) == "c1":
+                self.rook_start_coords = Coords.init_from_str("a1")
+                self.rook_end_coords = Coords.init_from_str("d1")
+        elif str(start_coords) == "e8":
+            if str(end_coords) == "g8":
+                self.rook_start_coords = Coords.init_from_str("h8")
+                self.rook_end_coords = Coords.init_from_str("f8")
+            elif str(end_coords) == "c8":
+                self.rook_start_coords = Coords.init_from_str("a8")
+                self.rook_end_coords = Coords.init_from_str("d8")
+
+
 
     def apply(self, board: Board):
         super().apply(board)
