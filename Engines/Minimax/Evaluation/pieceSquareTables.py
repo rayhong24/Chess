53ffaac77e3a0a764151e3e58dafd18cac0de5e0
ray hong <rayhong24@github.com>
2025-08-15 14:50:18 -0700

    Use evaluator class in minimax engine

--- Engines/Minimax/Evaluation/pieceSquareTables.py
+++ Engines/Minimax/Evaluation/pieceSquareTables.py
@@ -1,4 +1,4 @@
-from pieceSquareTableValues import table_values
+from .pieceSquareTableValues import table_values
 
 from Pieces.piece import Piece
 from enums import Colour
@@ -6,11 +6,11 @@ from coords import Coords
 
 class PieceSquareTables:
     def get_value(self, piece: Piece, coords: Coords):
-        piece_table_values = table_values[type(Piece)]
-        if piece.colour == Colour.WHITE:
-            return piece_table_values[coords.rank-1][coords.file.value]
+        piece_table_values = table_values[type(piece)]
+        if piece.colour == Colour.BLACK:
+            return -piece_table_values[coords.rank-1][coords.file.value]
         else:
-            return -piece_table_values[8-coords.rank][coords.file.value]
+            return piece_table_values[8-coords.rank][coords.file.value]
         
 
 
