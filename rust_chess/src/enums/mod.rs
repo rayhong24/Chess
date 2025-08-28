6127c93a18f85744e9b6dc43bc4c5d71034289d1
Ray Hong <rayhong24@gmail.com>
2025-08-28 11:05:18 -0700

    Port enums and coords to rust. Added basic testing

---
+++ rust_chess/src/enums/mod.rs
@@ -0,0 +1,6 @@
+pub mod file;
+pub mod colour;
+
+
+pub use file::File;
+pub use colour::Colour;
--- rust_chess/src/lib.rs
+++ rust_chess/src/lib.rs
@@ -1,14 +1,20 @@
-use pyo3::prelude::*;
+pub mod coords;
+pub mod enums;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+// #[cfg(not(test))]
+// use pyo3::prelude::*;
 
-/// A Python module implemented in Rust.
-#[pymodule]
-fn rust_chess(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    Ok(())
-}
+// /// Formats the sum of two numbers as string.
+// #[cfg(not(test))]
+// #[pyfunction]
+// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+//     Ok((a + b).to_string())
+// }
+
+// /// A Python module implemented in Rust.
+// #[cfg(not(test))]
+// #[pymodule]
+// fn rust_chess(m: &Bound<'_, PyModule>) -> PyResult<()> {
+//     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+//     Ok(())
+// }
