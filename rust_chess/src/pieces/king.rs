6b675096a1ca1420813c90eca5bdd7f754121495
Ray Hong <rayhong24@gmail.com>
2025-08-28 12:17:28 -0700

    Define piece trait and king struct

---
+++ rust_chess/src/pieces/king.rs
@@ -0,0 +1,37 @@
+use std::fmt::DebugSet;
+
+use crate::enums::Colour;
+use crate::coords::Coords;
+use crate::pieces::Piece;
+
+#[derive(Debug, Clone)]
+pub struct King {
+    pub colour: Colour,
+    pub position: Coords,
+}
+
+impl Piece for King {
+    fn colour(&self) -> Colour {
+        self.colour
+    }
+    fn get_representation(&self) -> char {
+        match self.colour {
+            Colour::White => 'K',
+            Colour::Black => 'k',
+        }
+    }
+    fn get_destination_coords(&self, from: Coords) -> Vec<Coords> {
+        let mut destinations = Vec::new();
+        for (rank_diff, file_diff) in &[
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),          (0, 1),
+            (1, -1),  (1, 0),  (1, 1),
+        ] {
+            if let Some(new_coords) = from.get_neighbour(*rank_diff, *file_diff) {
+                destinations.push(new_coords);
+            }
+        }
+
+        return destinations;
+    }
+}
