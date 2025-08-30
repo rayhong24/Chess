a081337596a56dc0def723dbadd61cea82474b7d
Ray Hong <rayhong24@gmail.com>
2025-08-30 14:59:05 -0700

    Move unit tests under test modules that compile only during test builds

--- rust_chess/src/pieces/knight.rs
+++ rust_chess/src/pieces/knight.rs
@@ -1,4 +1,4 @@
-use crate::enums::{Colour, File};
+use crate::enums::Colour;
 use crate::coords::Coords;
 use crate::moves::move_ray::MoveRay;
 use crate::pieces::Piece;
@@ -49,33 +49,39 @@ impl Piece for Knight {
 }
 
 
-#[test]fn test_knight_representation() {
-    let white_knight = Knight::new(Colour::White);
-    let black_knight = Knight::new(Colour::Black);  
-    assert_eq!(white_knight.get_representation(), 'N');
-    assert_eq!(black_knight.get_representation(), 'n');
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::enums::File;
 
-#[test]
-fn test_knight_moves_initial() {
-    let knight = Knight::new(Colour::White);
-    let coords = Coords::new(1, File::B);
-    let moves = knight.get_move_rays(coords);
+    #[test]fn test_knight_representation() {
+        let white_knight = Knight::new(Colour::White);
+        let black_knight = Knight::new(Colour::Black);  
+        assert_eq!(white_knight.get_representation(), 'N');
+        assert_eq!(black_knight.get_representation(), 'n');
+    }
 
-    // Knight should have 8 move rays (L-shaped moves)
-    assert_eq!(moves.len(), 8);
+    #[test]
+    fn test_knight_moves_initial() {
+        let knight = Knight::new(Colour::White);
+        let coords = Coords::new(1, File::B);
+        let moves = knight.get_move_rays(coords);
 
-    // Check each L-shaped move
-    let move_positions = [
-        (2, 1), (2, -1),
-        (1, 2), (1, -2),
-        (-1, 2), (-1, -2),
-        (-2, 1), (-2, -1)
-    ];
+        // Knight should have 8 move rays (L-shaped moves)
+        assert_eq!(moves.len(), 8);
 
-    for (rank_diff, file_diff) in &move_positions {
-        let mv = moves.iter().find(|m| m.rank_diff == *rank_diff && m.file_diff == *file_diff).unwrap();
-        assert_eq!(mv.dist, 1);
-        assert!(mv.capture_allowed && !mv.capture_forced);
+        // Check each L-shaped move
+        let move_positions = [
+            (2, 1), (2, -1),
+            (1, 2), (1, -2),
+            (-1, 2), (-1, -2),
+            (-2, 1), (-2, -1)
+        ];
+
+        for (rank_diff, file_diff) in &move_positions {
+            let mv = moves.iter().find(|m| m.rank_diff == *rank_diff && m.file_diff == *file_diff).unwrap();
+            assert_eq!(mv.dist, 1);
+            assert!(mv.capture_allowed && !mv.capture_forced);
+        }
     }
 }
